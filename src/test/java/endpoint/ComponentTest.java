4c7fb7c9fc4b4de30af92a14e66ec76a566d7260
zkuru <r.abuzyarova@gmail.com>
2019-10-07 23:56:09 +0300

    add db.props instead of profiles.

--- src/test/java/endpoint/ComponentTest.java
+++ src/test/java/endpoint/ComponentTest.java
@@ -1,13 +1,14 @@
 package endpoint;
 
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @WebAppConfiguration
-@ActiveProfiles("h2")
 @ContextConfiguration("classpath:spring-web-servlet.xml")
 
 @Target(ElementType.TYPE)
